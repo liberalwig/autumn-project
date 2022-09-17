a4bde95320770a2e6665b22c30f53622bdcf6ca1
최승은 <liberalfilm@gmail.com>
2022-09-18 00:37:58 +0900

    2022-09-18-Sun 00:37

---
+++ src/main/java/Controller/MainController.java
@@ -0,0 +1,12 @@
+package Controller;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+
+public class MainController {
+
+    public String main() {
+        System.out.println("MainController");
+
+        return "";
+    }
+}
