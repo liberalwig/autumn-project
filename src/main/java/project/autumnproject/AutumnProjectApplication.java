712288abccc443b82dc9fe226fd7cb10b5bc27a4
최승은 <liberalfilm@gmail.com>
2022-09-18 00:31:19 +0900

    2022-09-18-Sun 00:27

---
+++ src/main/java/project/autumnproject/AutumnProjectApplication.java
@@ -0,0 +1,14 @@
+package project.autumnproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutumnProjectApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AutumnProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/autumnproject/AutumnProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package project.autumnproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutumnProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
