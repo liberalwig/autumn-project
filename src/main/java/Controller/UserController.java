dae8b66f49a7155c41dee7ccead42e661d08cbc1
최승은 <liberalfilm@gmail.com>
2022-09-18 23:03:15 +0900

    2022-09-18-Sun 23:02

--- src/main/java/Controller/UserController.java
+++ src/main/java/Controller/UserController.java
@@ -1,4 +1,18 @@
 package Controller;
 
+import Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class UserController {
+
+     final
+    UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
 }
--- src/main/java/Service/UserService.java
+++ src/main/java/Service/UserService.java
@@ -1,4 +1,16 @@
 package Service;
 
+import Dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    final
+    UserDao userDao;
+
+    public UserService(UserDao userDao) {
+        this.userDao = userDao;
+    }
 }
